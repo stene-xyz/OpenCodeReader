70a635b5aa9c46e5d93da54afb5365d5d756265f
Johnny Stene <jhonnystene@protonmail.com>
2021-05-09 20:48:13 -0700

    Rename Elements

--- OpenCodeReader/OpenCodeReader/Form1.Designer.cs
+++ OpenCodeReader/OpenCodeReader/Form1.Designer.cs
@@ -34,13 +34,13 @@ namespace OpenCodeReader
             this.PortID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ScanEngine = new System.Windows.Forms.Button();
+            this.ScanABS = new System.Windows.Forms.Button();
             this.ScanResults = new System.Windows.Forms.ListView();
             this.label3 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TerminalListView = new System.Windows.Forms.ListView();
+            this.TerminalSend = new System.Windows.Forms.Button();
+            this.TerminalInput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ConnectButton
@@ -82,30 +82,30 @@ namespace OpenCodeReader
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(11, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 15);
             this.label2.TabIndex = 5;
             this.label2.Text = "Serial Port Configuration";
             // 
-            // button1
+            // ScanEngine
             // 
-            this.button1.Location = new System.Drawing.Point(11, 181);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Scan Engine";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ScanEngine.Location = new System.Drawing.Point(11, 181);
+            this.ScanEngine.Name = "ScanEngine";
+            this.ScanEngine.Size = new System.Drawing.Size(93, 23);
+            this.ScanEngine.TabIndex = 6;
+            this.ScanEngine.Text = "Scan Engine";
+            this.ScanEngine.UseVisualStyleBackColor = true;
+            this.ScanEngine.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // ScanABS
             // 
-            this.button2.Location = new System.Drawing.Point(180, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Scan ABS";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ScanABS.Location = new System.Drawing.Point(180, 181);
+            this.ScanABS.Name = "ScanABS";
+            this.ScanABS.Size = new System.Drawing.Size(93, 23);
+            this.ScanABS.TabIndex = 7;
+            this.ScanABS.Text = "Scan ABS";
+            this.ScanABS.UseVisualStyleBackColor = true;
             // 
             // ScanResults
             // 
@@ -119,49 +119,49 @@ namespace OpenCodeReader
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 211);
+            this.label3.Location = new System.Drawing.Point(11, 211);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 15);
             this.label3.TabIndex = 9;
             this.label3.Text = "OBD-II Terminal";
             // 
-            // listView1
+            // TerminalListView
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(11, 229);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(262, 97);
-            this.listView1.TabIndex = 10;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.TerminalListView.HideSelection = false;
+            this.TerminalListView.Location = new System.Drawing.Point(11, 229);
+            this.TerminalListView.Name = "TerminalListView";
+            this.TerminalListView.Size = new System.Drawing.Size(262, 97);
+            this.TerminalListView.TabIndex = 10;
+            this.TerminalListView.UseCompatibleStateImageBehavior = false;
             // 
-            // button3
+            // TerminalSend
             // 
-            this.button3.Location = new System.Drawing.Point(198, 332);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Send";
-            this.button3.UseVisualStyleBackColor = true;
+            this.TerminalSend.Location = new System.Drawing.Point(198, 332);
+            this.TerminalSend.Name = "TerminalSend";
+            this.TerminalSend.Size = new System.Drawing.Size(75, 23);
+            this.TerminalSend.TabIndex = 11;
+            this.TerminalSend.Text = "Send";
+            this.TerminalSend.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // TerminalInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 333);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 23);
-            this.textBox1.TabIndex = 12;
+            this.TerminalInput.Location = new System.Drawing.Point(11, 333);
+            this.TerminalInput.Name = "TerminalInput";
+            this.TerminalInput.Size = new System.Drawing.Size(181, 23);
+            this.TerminalInput.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(287, 368);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.TerminalInput);
+            this.Controls.Add(this.TerminalSend);
+            this.Controls.Add(this.TerminalListView);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.ScanResults);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ScanABS);
+            this.Controls.Add(this.ScanEngine);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PortID);
@@ -181,13 +181,13 @@ namespace OpenCodeReader
         private System.Windows.Forms.TextBox PortID;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ScanEngine;
+        private System.Windows.Forms.Button ScanABS;
         private System.Windows.Forms.ListView ScanResults;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView TerminalListView;
+        private System.Windows.Forms.Button TerminalSend;
+        private System.Windows.Forms.TextBox TerminalInput;
     }
 }
 
