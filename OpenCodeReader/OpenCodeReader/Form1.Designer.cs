de8834ac8cb60602b1a0288b049a4aab84b68e18
Johnny Stene <jhonnystene@protonmail.com>
2021-05-10 09:01:07 -0700

    Add baudrate selector and start coding UI

--- OpenCodeReader/OpenCodeReader/Form1.Designer.cs
+++ OpenCodeReader/OpenCodeReader/Form1.Designer.cs
@@ -41,6 +41,9 @@ namespace OpenCodeReader
             this.TerminalListView = new System.Windows.Forms.ListView();
             this.TerminalSend = new System.Windows.Forms.Button();
             this.TerminalInput = new System.Windows.Forms.TextBox();
+            this.Baudrate9600 = new System.Windows.Forms.RadioButton();
+            this.Baudrate38400 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ConnectButton
@@ -51,6 +54,7 @@ namespace OpenCodeReader
             this.ConnectButton.TabIndex = 0;
             this.ConnectButton.Text = "Connect";
             this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
             // 
             // DisconnectButton
             // 
@@ -60,7 +64,7 @@ namespace OpenCodeReader
             this.DisconnectButton.TabIndex = 1;
             this.DisconnectButton.Text = "Disconnect";
             this.DisconnectButton.UseVisualStyleBackColor = true;
-            this.DisconnectButton.Click += new System.EventHandler(this.button2_Click);
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
             // 
             // PortID
             // 
@@ -73,7 +77,7 @@ namespace OpenCodeReader
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 53);
+            this.label1.Location = new System.Drawing.Point(9, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 15);
             this.label1.TabIndex = 4;
@@ -90,17 +94,16 @@ namespace OpenCodeReader
             // 
             // ScanEngine
             // 
-            this.ScanEngine.Location = new System.Drawing.Point(11, 181);
+            this.ScanEngine.Location = new System.Drawing.Point(9, 201);
             this.ScanEngine.Name = "ScanEngine";
             this.ScanEngine.Size = new System.Drawing.Size(93, 23);
             this.ScanEngine.TabIndex = 6;
             this.ScanEngine.Text = "Scan Engine";
             this.ScanEngine.UseVisualStyleBackColor = true;
-            this.ScanEngine.Click += new System.EventHandler(this.button1_Click);
             // 
             // ScanABS
             // 
-            this.ScanABS.Location = new System.Drawing.Point(180, 181);
+            this.ScanABS.Location = new System.Drawing.Point(178, 201);
             this.ScanABS.Name = "ScanABS";
             this.ScanABS.Size = new System.Drawing.Size(93, 23);
             this.ScanABS.TabIndex = 7;
@@ -110,7 +113,7 @@ namespace OpenCodeReader
             // ScanResults
             // 
             this.ScanResults.HideSelection = false;
-            this.ScanResults.Location = new System.Drawing.Point(12, 71);
+            this.ScanResults.Location = new System.Drawing.Point(10, 91);
             this.ScanResults.Name = "ScanResults";
             this.ScanResults.Size = new System.Drawing.Size(261, 104);
             this.ScanResults.TabIndex = 8;
@@ -119,7 +122,7 @@ namespace OpenCodeReader
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 211);
+            this.label3.Location = new System.Drawing.Point(9, 231);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 15);
             this.label3.TabIndex = 9;
@@ -128,7 +131,7 @@ namespace OpenCodeReader
             // TerminalListView
             // 
             this.TerminalListView.HideSelection = false;
-            this.TerminalListView.Location = new System.Drawing.Point(11, 229);
+            this.TerminalListView.Location = new System.Drawing.Point(9, 249);
             this.TerminalListView.Name = "TerminalListView";
             this.TerminalListView.Size = new System.Drawing.Size(262, 97);
             this.TerminalListView.TabIndex = 10;
@@ -136,7 +139,7 @@ namespace OpenCodeReader
             // 
             // TerminalSend
             // 
-            this.TerminalSend.Location = new System.Drawing.Point(198, 332);
+            this.TerminalSend.Location = new System.Drawing.Point(196, 352);
             this.TerminalSend.Name = "TerminalSend";
             this.TerminalSend.Size = new System.Drawing.Size(75, 23);
             this.TerminalSend.TabIndex = 11;
@@ -145,16 +148,52 @@ namespace OpenCodeReader
             // 
             // TerminalInput
             // 
-            this.TerminalInput.Location = new System.Drawing.Point(11, 333);
+            this.TerminalInput.Location = new System.Drawing.Point(9, 353);
             this.TerminalInput.Name = "TerminalInput";
             this.TerminalInput.Size = new System.Drawing.Size(181, 23);
             this.TerminalInput.TabIndex = 12;
+            // 
+            // Baudrate9600
+            // 
+            this.Baudrate9600.AutoSize = true;
+            this.Baudrate9600.Checked = true;
+            this.Baudrate9600.Location = new System.Drawing.Point(163, 51);
+            this.Baudrate9600.Name = "Baudrate9600";
+            this.Baudrate9600.Size = new System.Drawing.Size(49, 19);
+            this.Baudrate9600.TabIndex = 13;
+            this.Baudrate9600.TabStop = true;
+            this.Baudrate9600.Text = "9600";
+            this.Baudrate9600.UseVisualStyleBackColor = true;
+            this.Baudrate9600.CheckedChanged += new System.EventHandler(this.Baudrate9600_CheckedChanged);
+            // 
+            // Baudrate38400
+            // 
+            this.Baudrate38400.AutoSize = true;
+            this.Baudrate38400.Location = new System.Drawing.Point(218, 51);
+            this.Baudrate38400.Name = "Baudrate38400";
+            this.Baudrate38400.Size = new System.Drawing.Size(55, 19);
+            this.Baudrate38400.TabIndex = 14;
+            this.Baudrate38400.Text = "38400";
+            this.Baudrate38400.UseVisualStyleBackColor = true;
+            this.Baudrate38400.CheckedChanged += new System.EventHandler(this.Baudrate38400_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Baudrate";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 368);
+            this.ClientSize = new System.Drawing.Size(287, 386);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Baudrate38400);
+            this.Controls.Add(this.Baudrate9600);
             this.Controls.Add(this.TerminalInput);
             this.Controls.Add(this.TerminalSend);
             this.Controls.Add(this.TerminalListView);
@@ -188,6 +227,9 @@ namespace OpenCodeReader
         private System.Windows.Forms.ListView TerminalListView;
         private System.Windows.Forms.Button TerminalSend;
         private System.Windows.Forms.TextBox TerminalInput;
+        private System.Windows.Forms.RadioButton Baudrate9600;
+        private System.Windows.Forms.RadioButton Baudrate38400;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- OpenCodeReader/OpenCodeReader/Form1.cs
+++ OpenCodeReader/OpenCodeReader/Form1.cs
@@ -1,40 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace OpenCodeReader
 {
     public partial class Form1 : Form
     {
+        private SerialPort port;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private int getBaudRate()
         {
-
+            if (Baudrate38400.Checked) return 38400;
+            else return 9600;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Baudrate9600_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Baudrate9600.Checked) Baudrate38400.Checked = false;
+            else Baudrate38400.Checked = true;
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void Baudrate38400_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Baudrate38400.Checked) Baudrate9600.Checked = false;
+            else Baudrate9600.Checked = true;
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void ConnectButton_Click(object sender, EventArgs e)
         {
+            port = new SerialPort(PortID.Text, getBaudRate(), Parity.None, 8, StopBits.One);
+            port.Open();
+        }
 
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            port.Close();
+            port = null;
         }
     }
 }
